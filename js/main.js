8039ebbc350814f91f1c085bd64a1ed958926767
Yevhen Martianov <jonny.freeman321@gmail.com>
2020-12-30 15:16:46 +0200

    intersection observer

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
         closeModalClick: document.querySelectorAll("[close-modal]"),
         modal: document.querySelector("[modal]"),
     }
-    
+
     refs.openModalClick.addEventListener('click', toggleModal);
     for (ref of refs.closeModalClick) {
         ref.addEventListener('click', toggleModal);
@@ -15,3 +15,26 @@
 
 
 })();
+
+
+//#################################### Intersection observer ##############################//
+
+function onEntry(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            console.log('HereI am');
+            entry.target.style.transform = 'translateY(0)';
+        }
+    })
+}
+
+
+let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.2,
+};
+
+let target = Array.from(document.getElementsByTagName('section'));
+let observer = new IntersectionObserver(onEntry, options);
+target.forEach(element => observer.observe(element));
